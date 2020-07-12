80845bc369c203137c654284d89721f6bc8e6492
Archangel354 <rduncan354@gmail.com>
2020-07-11 23:50:27 -0400

    In Main Activity, trying to pass a string to UpdateWidgetService Created MyIntentService, may have to delete this The biggest change of all.  I updated UpdateWidgetService to be JobIntentService.  God help me! AndroidManifest.xml; added WAKE_LOCK permissioin and added permission BIND_JOB_SERVICE to UpdateWidgetService

--- app/src/main/java/com/example/duncanro/widgetexample/MainActivity.java
+++ app/src/main/java/com/example/duncanro/widgetexample/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.duncanro.widgetexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -14,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, UpdateWidgetService.class);
+        intent.putExtra(EXTRA_RECIPENAME, "vanilla");
+        startService(intent);
 
 //        Intent intent = new Intent(this,UpdateWidgetService.class);
 //        intent.putExtra(EXTRA_RECIPENAME, widgetText);
--- app/src/main/java/com/example/duncanro/widgetexample/UpdateWidgetService.java
+++ app/src/main/java/com/example/duncanro/widgetexample/UpdateWidgetService.java
@@ -6,19 +6,37 @@ import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.RemoteViews;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.JobIntentService;
 
 import java.util.Random;
 
 import static com.example.duncanro.widgetexample.MainActivity.EXTRA_RECIPENAME;
 
-public class UpdateWidgetService extends Service {
+public class UpdateWidgetService extends JobIntentService {
     private static final String LOG = "com.example.duncanro.widgetexample";
     public static final String ACTION_UPDATE_PLANT_WIDGETS = "com.example.duncanro.widgetexample.action.update_plant_widgets";
 
-    @Override
+    /**
+     * Unique job ID for this service.
+     */
+    static final int JOB_ID = 1000;
+
+    /**
+     * Convenience method for enqueuing work in to this service.
+     */
+    static void enqueueWork(Context context, Intent work) {
+        enqueueWork(context, UpdateWidgetService.class, JOB_ID, work);
+    }
+
+       @Override
     public void onStart(Intent intent, int startId) {
         AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this
                 .getApplicationContext());
@@ -33,14 +51,15 @@ public class UpdateWidgetService extends Service {
         for (int widgetId : allWidgetIds) {
             // create some random data
             int number = (new Random().nextInt(100));
+            String widgetText = "LoveBug!!";
 
             RemoteViews remoteViews = new RemoteViews(this
                     .getApplicationContext().getPackageName(),
                     R.layout.new_app_widget);
             Log.w("WidgetExample", String.valueOf(number));
             // Set the text
-            remoteViews.setTextViewText(R.id.appwidget_text,
-                    "Random: " + String.valueOf(number));
+            //remoteViews.setTextViewText(R.id.appwidget_text,"Random: " + String.valueOf(number));
+            //remoteViews.setTextViewText(R.id.textView3,widgetText);
 
             // Register an onClickListener
             Intent clickIntent = new Intent(this.getApplicationContext(),
@@ -50,10 +69,9 @@ public class UpdateWidgetService extends Service {
             clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS,
                     allWidgetIds);
 
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(
-                    getApplicationContext(), 0, clickIntent,
-                    PendingIntent.FLAG_UPDATE_CURRENT);
-            remoteViews.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, clickIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+            //remoteViews.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
+            remoteViews.setOnClickPendingIntent(R.id.textView3, pendingIntent);
             appWidgetManager.updateAppWidget(widgetId, remoteViews);
         }
         stopSelf();
@@ -61,23 +79,30 @@ public class UpdateWidgetService extends Service {
         super.onStart(intent, startId);
     }
 
+
+
     @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    protected void onHandleWork(@NonNull Intent intent) {
+        Log.i("SimpleJobIntentService", "Executing work: " + intent);
+
     }
 
-
-
-
-    /**
-     * Starts this service to perform UpdatePlantWidgets action with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
-    public static void startActionUpdateWidget(Context context) {
-        Intent intent = new Intent(context, UpdateWidgetService.class);
-        intent.setAction(ACTION_UPDATE_PLANT_WIDGETS);
-        context.startService(intent);
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        toast("All work complete");
     }
-}
+
+    @SuppressWarnings("deprecation")
+    final Handler mHandler = new Handler();
+
+    // Helper for showing tests
+    void toast(final CharSequence text) {
+        mHandler.post(new Runnable() {
+            @Override public void run() {
+                Toast.makeText(UpdateWidgetService.this, text, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+   }
